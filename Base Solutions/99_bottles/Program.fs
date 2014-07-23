6a6f9303b691567902719dcfa781948b0bac7bb7
Grant Crofton <grantac@hotmail.com>
2014-07-24 00:30:10 +0100

    Added base solutions

---
+++ Base Solutions/99_bottles/Program.fs
@@ -0,0 +1,12 @@
+﻿[<EntryPoint>]
+let main argv = 
+    for bottles in [99..-1..1] do
+        if bottles > 1 then
+            if bottles < 99 then printfn "%i bottles of beer on the wall.\n" bottles
+            printfn "%i bottles of beer on the wall, %i bottles of beer." bottles bottles
+            printf "Take one down and pass it around, "
+        else
+            printfn "1 bottle of beer on the wall.\n"
+            printfn "1 bottle of beer on the wall, 1 bottle of beer."
+            printfn "Go to the store and buy some more, 99 bottles of beer on the wall."
+    0
---
+++ Base Solutions/FizzBuzz.fsx
@@ -0,0 +1,9 @@
+﻿for index in [1..100] do
+    if index % 3 = 0 && index % 5 = 0 then
+        printfn "FizzBuzz"
+    else if index % 3 = 0 then
+        printfn "Fizz"
+    else if index % 5 = 0 then
+        printfn "Buzz"
+    else
+        printfn "%i" index
